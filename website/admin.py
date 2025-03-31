96e967d4f29224aa1ed92e02fdfc3f850678e319
Levi Kuhaulua <126765430+LeviKuhaulua@users.noreply.github.com>
2025-03-31 08:38:32 -1000

    Refactor: rename image to logo and directly integrated imageField into Program model

--- website/admin.py
+++ website/admin.py
@@ -1,22 +1,7 @@
 from django.contrib import admin
-from .models import Program, Image
+from .models import Program
 
 # Register your models here.
-
-@admin.register(Image)
-class ImageAdmin(admin.ModelAdmin): 
-    def get_actions(self, request): 
-        actions = super().get_actions(request)
-
-        if request.user.username[0].upper() != "J":
-
-            if "delete_selected" in actions: 
-                del actions["delete_selected"]
-        
-        return actions 
-    
-    def __str__(self): 
-        return self.name
 
 @admin.register(Program)
 class ProgramAdmin(admin.ModelAdmin):
--- website/models.py
+++ website/models.py
@@ -9,26 +9,21 @@ class File(models.Model):
     def __str__(self): 
         return self.filename
     
-class Image(models.Model): 
-    name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='website.File/bytes/filename/mimetype', 
-                              null=True, help_text='''Please compress image and convert type to webp before uploading. 
-                              https://imagecompressor.com, 
-                              https://cloudconvert.com/webp-converter''')
-    
-    def __str__(self): 
-        return self.name
-    
-    def delete(self, *args, **kwargs): 
-        super(Image, self).delete(*args, **kwargs)
-        File.objects.filter(filename=self.image.name).delete()
-
 
 class Program(models.Model):
     name = models.CharField(max_length=100, unique=True, help_text="Name of Club/Program")
-    image = models.ForeignKey(Image, null=True, on_delete=models.SET_NULL, help_text="Club Logo")
+    logo = models.ImageField(
+        upload_to="website.File/bytes/filename/mimetype",
+        null=True,
+        help_text='''
+        Please compress the image and convert type to webp before uploading.
+        https://imagecompressor.com,
+        https://cloudconvert.com/webp-converter
+        '''
+    )
     link_url = models.URLField(help_text="Enter full link to the club/program website", blank=True)
     link_name = models.CharField(max_length=100, help_text="Short label for the URL, e.g \"Visit Site\"", blank=True)
+
     
     def __str__(self): 
         return self.name
