3503b5e22ae13af4f7cb76582acbcd5695a38281
Benny <76574420+bennytrieu@users.noreply.github.com>
2025-03-31 17:01:47 -1000

    made the CarouselImage model but haven't tested it yet

--- website/admin.py
+++ website/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from .models import Program
+from .models import CarouselImage
 
 # Register your models here.
 
 @admin.register(Program)
 class ProgramAdmin(admin.ModelAdmin):
     # Leaving this here in case we want additional functionality
+    pass
+
+@admin.register(CarouselImage)
+class CarouselImageAdmin(admin.ModelAdmin):
     pass
