62b9d5fb743d3c44b2dc92d4aefd56d4630ebf1f
Levi Kuhaulua <126765430+LeviKuhaulua@users.noreply.github.com>
2025-06-22 11:16:41 -1000

    Impl: Removed focus-within state, no image

--- website/static/js/cohorts.js
+++ website/static/js/cohorts.js
@@ -8,19 +8,14 @@ membersAll.forEach((value) => {
       value.setAttribute('aria-expanded', 'true');
       child.classList.remove(
         'group-hover:translate-y-[60%]', 
-        'group-focus-within:translate-y-[60%]',
         'translate-y-[80%]',
       );
     } else {
       value.setAttribute('aria-expanded', 'false');
       child.classList.add(
         'group-hover:translate-y-[60%]',
-        'group-focus-within:translate-y-[60%]',
         'translate-y-[80%]',
       );
-      // Prevents user from being stuck in focus state when
-      // they're using their mouse (only mouse) right after
-      value.blur();
     }
   });
 })
